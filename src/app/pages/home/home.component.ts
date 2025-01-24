dfdec94ea2ce683dbc51dc5e7c4fa9d98d65f997
PedroHenriqueMG <pedrohdefsouza@gmail.com>
2025-01-24 20:15:31 -0300

    feat: implement scroll interation in home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
   selector: "app-home",
@@ -7,4 +7,35 @@ import { Component } from "@angular/core";
   templateUrl: "./home.component.html",
   styleUrl: "./home.component.scss",
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  typedText = "";
+  fullText = `Olá, me chamo Pedro Henrique,<br>sou desenvolvedor Fullstack, seja bem-vindo ao meu portifólio!<br> Fique a vontade para navegar pelo site!`;
+  isScroll = false;
+  hasStartedTyping = false;
+
+  ngOnInit(): void {}
+
+  startTyping(): void {
+    if (this.hasStartedTyping) return;
+    this.hasStartedTyping = true;
+
+    let index = 0;
+    const interval = setInterval(() => {
+      if (index < this.fullText.length) {
+        this.typedText += this.fullText[index];
+        index++;
+      } else {
+        clearInterval(interval);
+      }
+    }, 100);
+  }
+  scrollDown(teste: Event) {
+    const target = teste.target as HTMLDivElement;
+    if (target.scrollTop > 60) {
+      this.isScroll = true;
+      this.startTyping();
+    } else {
+      this.isScroll = false;
+    }
+  }
+}
