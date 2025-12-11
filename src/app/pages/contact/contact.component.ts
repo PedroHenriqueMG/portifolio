1d81ec230413ccbbed307b7890620ab24e6cc017
PedroHneriqueMG <pedrohdefsouza@gmail.com>
2025-12-11 12:46:19 -0300

    feat: update contact component to open popup on button click; enhance styling and layout in about and home sections

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { ContactPopupService } from "../../services/contact-popup.service";
 
 @Component({
   selector: "app-contact",
@@ -7,4 +8,10 @@ import { Component } from "@angular/core";
   templateUrl: "./contact.component.html",
   styleUrl: "./contact.component.scss",
 })
-export class ContactComponent {}
+export class ContactComponent {
+  constructor(private contactPopupService: ContactPopupService) {}
+
+  openContactPopup() {
+    this.contactPopupService.openPopup();
+  }
+}
